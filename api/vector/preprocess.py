39eb6ef9f6cea7d1dd65dfb45fec06ef693d1ce5
Nicola Richli <nicola@akenza.io>
2025-07-11 17:34:40 +0200

    feat(api): update script to beautify html files

--- api/vector/preprocess.py
+++ api/vector/preprocess.py
@@ -26,12 +26,38 @@ def pdf_to_html(html_title, pdf_path, html_path, out_dir):
         f"<!DOCTYPE html><html><head>"
         f"<meta charset='utf-8'><title>{html_title}</title>"
         f"<link rel='stylesheet' href='{BASE_URL}/static.css'>"
-        "</head><body></body></html>",
+        "</head><body style='background-color: rgb(243 244 246);'></body></html>",
         "html.parser"
     )    
 
     body = html.body
-    
+
+    text_container = html.new_tag("div", **{"class": "text-container"})
+
+    # Header-Container
+    header = html.new_tag("div", **{"class": "header"})
+    # Logo-Link
+    logo = html.new_tag("a", href="/", **{"class": "logo"})
+    img = html.new_tag("img", src="/logo-colored.webp", alt="Logo", width="28", height="28")
+    title = html.new_tag("div"); title.string = "Rahmenabkommen GPT"
+    logo.append(img); logo.append(title)
+    header.append(logo)
+
+    # Help icon
+    help = html.new_tag('a', href="/help", dataDiscover="true", **{"class": "help-button"})
+    help_icon = html.new_tag('svg', xmlns="http://www.w3.org/2000/svg", width="26", height="26", fill="currentColor", viewBox="0 0 256 256")
+    help_icon_path = html.new_tag('path', d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z")
+    help_icon.append(help_icon_path)
+    help.append(help_icon)
+    header.append(help)
+
+    # Dark-Mode-Button
+    #dm_btn = html.new_tag("button", **{"aria-label": "Toggle dark mode" })
+    #dm_btn.append(BeautifulSoup(darkmode_svg, "html.parser"))
+    #header.append(dm_btn)
+
+    body.append(header)    
+    body.append(text_container)    
     # ID-Counter für Referenzen
     counters = {"h1": 0, "h2": 0, "p": 0}
 
@@ -41,7 +67,7 @@ def pdf_to_html(html_title, pdf_path, html_path, out_dir):
         counters["h2"] += 1
         if not page_header.get('id'):
             page_header['id'] = f"h{counters['h2']}"
-        body.append(page_header)
+        text_container.append(page_header)
 
         # Textblöcke im „dict“-Format
         page_dict = page.get_text("dict")
@@ -69,7 +95,7 @@ def pdf_to_html(html_title, pdf_path, html_path, out_dir):
                         counters[tag_name] += 1
                         tag['id'] = f"p{counters[tag_name]}"
 
-                    body.append(tag)
+                    text_container.append(tag)
 
     # Schreibe die Datei
     out_path = os.path.join(out_dir, html_path)
@@ -83,6 +109,7 @@ def extract_text_with_mapping(soup):
     text = ""
     mapping = []
     current_pos = 0
+
     for element in soup.find_all(['span', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
         element_text = element.get_text()
         start = current_pos
@@ -128,7 +155,6 @@ def make_html_title(filename: str) -> str:
     return base
 
 def build_and_save_vectorstore(pdf_dir, html_dir, output_path):
-
     pdf_paths = [os.path.join(pdf_dir, f) for f in os.listdir(pdf_dir) if f.endswith(".pdf")]
     print(f"PDFs gefunden: {len(pdf_paths)}")
     
